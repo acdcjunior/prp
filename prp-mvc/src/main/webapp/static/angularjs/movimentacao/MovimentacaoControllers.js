4ea04c12feafdd66d04a73d7788592e43d8f2890
acdcjunior <acdcjunior@gmail.com>
2014-06-29 20:46:35 -0300

    Adicao de alteracao de Movimentacoes em massa

--- prp-mvc/src/main/webapp/static/angularjs/movimentacao/MovimentacaoControllers.js
+++ prp-mvc/src/main/webapp/static/angularjs/movimentacao/MovimentacaoControllers.js
@@ -1,6 +1,6 @@
 
-app.controller('MovimentacaoListCtrl', ['$scope', '$routeParams', 'MovimentacoesFactory', 'MovimentacaoFactory', '$location',
-    function ($scope, $routeParams, MovimentacoesFactory, MovimentacaoFactory, $location) {
+app.controller('MovimentacaoListCtrl', ['$scope', '$routeParams', 'MovimentacoesFactory', 'MovimentacaoFactory', '$location', '$modal',
+    function ($scope, $routeParams, MovimentacoesFactory, MovimentacaoFactory, $location, $modal) {
         var parametroFiltroMes = $routeParams.anoMes;
         if (parametroFiltroMes) {
             $scope.filtroMes = parametroFiltroMes;
@@ -22,6 +22,95 @@ app.controller('MovimentacaoListCtrl', ['$scope', '$routeParams', 'Movimentacoes
         };
 
         $scope.movimentacoes = MovimentacoesFactory.query();
+
+        $scope.selecionarMovimentacao = function (mov) {
+            if (mov.selecionada) {
+                mov.selecionada = false;
+            } else {
+                mov.selecionada = true;
+            }
+        };
+
+        $scope.alterarMovimentacoesSelecionadas = function () {
+            var modalInstance = $modal.open({
+                templateUrl: 'movimentacao/movimentacao-list-alterar-selecionadas.html',
+                controller: 'ModalAlterarMovimentacoesSelecionadasCtrl',
+                resolve: {
+                    movimentacoesSelecionadas: function () {
+                        return $scope.movimentacoesSelecionadas;
+                    }
+                }
+            });
+            modalInstance.result.then(function () {
+                // recarrega lista de movimentacoes
+                // $scope.movimentacoes = MovimentacoesFactory.query();
+            }, function () {
+                // console.log('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        $scope.removerSelecoes = function (mov) {
+            for (var i = 0; i < $scope.movimentacoesSelecionadas.length; i++) {
+                $scope.movimentacoesSelecionadas[i].selecionada = false;
+            }
+        };
+    }
+]);
+
+app.controller('ModalAlterarMovimentacoesSelecionadasCtrl',
+    ['$scope', '$modalInstance', 'MovimentacaoFactory', '$location', '$modal', 'PrevisoesFactory', 'movimentacoesSelecionadas',
+    function ($scope, $modalInstance, MovimentacaoFactory, $location, $modal, PrevisoesFactory, movimentacoesSelecionadas) {
+
+        $scope.alts = {
+            alterarDescricao2: false,
+            alterarPrevisao: false,
+            descricao2: movimentacoesSelecionadas[0].descricao2,
+            previsaoSelecionada: movimentacoesSelecionadas[0].realiza
+        };
+
+        $scope.limparPrevisaoSelecionada = function () {
+            $scope.alts.previsaoSelecionada = undefined;
+        };
+
+        $scope.salvar = function () {
+            for (var i = 0; i < movimentacoesSelecionadas.length; i++) {
+                var mov = movimentacoesSelecionadas[i];
+                if ($scope.alts.alterarPrevisao) {
+                    mov.realiza = $scope.alts.previsaoSelecionada;
+                }
+                if ($scope.alts.alterarDescricao2) {
+                    mov.descricao2 = $scope.alts.descricao2;
+                }
+                if ($scope.alts.alterarPrevisao || $scope.alts.alterarDescricao2) {
+                    MovimentacaoFactory.update(mov);
+                }
+            }
+            $modalInstance.close();
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+        $scope.modalAlterarPrevisaoRealizada = function () {
+            var modalInstance = $modal.open({
+                templateUrl: 'movimentacao/movimentacao-detail-previsao.html',
+                controller: 'ModalAlterarPrevisaoRealizadaCtrl',
+                resolve: {
+                    mesInicial: function () {
+                        return movimentacoesSelecionadas[0].data.substring(0, 7);
+                    },
+                    previsaoAtual: function () {
+                        return undefined;
+                    }
+                }
+            });
+            modalInstance.result.then(function (previsaoSelecionada) {
+                $scope.alts.previsaoSelecionada = previsaoSelecionada;
+            }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        };
     }
 ]);
 
@@ -46,8 +135,11 @@ app.controller('MovimentacaoDetailCtrl', ['$scope', '$routeParams', 'Movimentaca
                 templateUrl: 'movimentacao/movimentacao-detail-previsao.html',
                 controller: 'ModalAlterarPrevisaoRealizadaCtrl',
                 resolve: {
-                    movimentacao: function () {
-                        return $scope.movimentacao;
+                    mesInicial: function () {
+                        return $scope.movimentacao.data.substring(0, 7);
+                    },
+                    previsaoAtual: function () {
+                        return $scope.movimentacao.realiza;
                     }
                 }
             });
@@ -61,13 +153,14 @@ app.controller('MovimentacaoDetailCtrl', ['$scope', '$routeParams', 'Movimentaca
     }
 ]);
 
-app.controller('ModalAlterarPrevisaoRealizadaCtrl', ['$scope', '$modalInstance', '$location', 'PrevisoesFactory', 'movimentacao',
-    function ($scope, $modalInstance, $location, PrevisoesFactory, movimentacao) {
-        $scope.filtroPrevisaoMes = movimentacao.data.substring(0, 7);
+app.controller('ModalAlterarPrevisaoRealizadaCtrl', ['$scope', '$modalInstance', '$location', 'PrevisoesFactory', 'mesInicial', 'previsaoAtual',
+    function ($scope, $modalInstance, $location, PrevisoesFactory, mesInicial, previsaoAtual) {
+
+        $scope.filtroPrevisaoMes = mesInicial;
+        $scope.previsaoAtual = previsaoAtual;
 
         $scope.previsoes = PrevisoesFactory.query();
 
-        $scope.previsaoAtual = movimentacao.realiza;
 
         $scope.selecionar = function (previsao) {
             $modalInstance.close(previsao);
@@ -83,6 +176,8 @@ app.controller('ModalAlterarPrevisaoRealizadaCtrl', ['$scope', '$modalInstance',
         };
     }
 ]);
+
+
 
 app.controller('MovimentacaoCreationCtrl', ['$scope', 'MovimentacoesFactory', '$location',
     function ($scope, MovimentacoesFactory, $location) {
