07bc1e3dbb40bb78d4d29ad38568d8ce5d4e362e
acdcjunior <acdcjunior@gmail.com>
2016-03-05 19:22:45 -0300

    chore(prp): add initial spring-boot version

---
+++ src/main/java/io/github/acdcjunior/boot/PRPApplication.java
@@ -0,0 +1,26 @@
+package io.github.acdcjunior.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PRPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PRPApplication.class, args);
+    }
+
+    @RestController
+    static class PRPApplicationController {
+
+        @RequestMapping("/")
+        public ResponseEntity<String> prp() {
+            return ResponseEntity.ok("Hey! " + String.join(";", "java", "8", "test") + " Now: " + java.time.ZonedDateTime.now());
+        }
+
+    }
+
+}
