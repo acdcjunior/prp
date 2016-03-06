6c2cc9de6ab7c7d834c1dc0f3662973a1dd8b36d
acdcjunior <acdcjunior@gmail.com>
2016-03-06 18:20:18 -0300

    chore(spring): rename boot class from PRPApplication to Application

--- src/test/java/io/github/acdcjunior/boot/ApplicationTest.java
+++ src/test/java/io/github/acdcjunior/boot/ApplicationTest.java
@@ -7,9 +7,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = PRPApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-public class PRPApplicationTests {
+public class ApplicationTest {
 
 	@Test
 	public void contextLoads() { }
