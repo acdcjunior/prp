1175bd0035a36ae9d70efb0e2e80ca309feeb357
acdcjunior <acdcjunior@gmail.com>
2016-03-13 20:47:14 -0300

    chore(prp-boot): configure max upload size

--- prp-boot/src/main/java/io/github/acdcjunior/prp/Application.java
+++ prp-boot/src/main/java/io/github/acdcjunior/prp/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.velocity.VelocityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver;
 
 @SpringBootApplication
@@ -19,6 +20,15 @@ public class Application {
         properties.applyToViewResolver(resolver);
         resolver.setLayoutUrl("layout/layout.vm");
         resolver.setContentType("text/html;charset=UTF-8");
+        return resolver;
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver createMultipartResolver() {
+        CommonsMultipartResolver resolver=new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("utf-8");
+        // the maximum file size in bytes: 15MB
+        resolver.setMaxUploadSize(15_000_000);
         return resolver;
     }
 
