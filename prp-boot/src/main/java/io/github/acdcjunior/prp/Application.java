de97f16d3c297db25c27985ad8f37a3432006929
acdcjunior <acdcjunior@gmail.com>
2016-03-13 19:14:43 -0300

    chore(prp-boot): use VelocityLayoutViewResolver as view resolver

--- prp-boot/src/main/java/io/github/acdcjunior/prp/Application.java
+++ prp-boot/src/main/java/io/github/acdcjunior/prp/Application.java
@@ -2,12 +2,24 @@ package io.github.acdcjunior.prp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.velocity.VelocityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(name = "velocityViewResolver")
+    public VelocityLayoutViewResolver velocityViewResolver(VelocityProperties properties) {
+        VelocityLayoutViewResolver resolver = new VelocityLayoutViewResolver();
+        properties.applyToViewResolver(resolver);
+        resolver.setLayoutUrl("layout/layout.vm");
+        resolver.setContentType("text/html;charset=UTF-8");
+        return resolver;
     }
 
 }
