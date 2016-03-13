a3d3cee26d4f82610443df6d33018c309e70d2e5
acdcjunior <acdcjunior@gmail.com>
2016-03-13 20:38:29 -0300

    chore(prp-mvc): remove unused files

--- prp-mvc/src/test/javascript/prp/console.js
+++
@@ -1,9 +0,0 @@
-/* Define a variavel "console" para os testes que rodam no HtmlUnit nao falharem */
-if (typeof window.console == "undefined") {
-	window.console = {
-		log : function() { },
-		error : function() { },
-		debug : function() { },
-		dir : function() { }
-	};
-}
