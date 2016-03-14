61bf6a6def6d49794482ae5af4722c20e29fba89
acdcjunior <acdcjunior@gmail.com>
2016-03-13 21:00:03 -0300

    chore(prp-boot): configure jackson date serialization

--- prp-boot/src/main/java/io/github/acdcjunior/prp/Application.java
+++ prp-boot/src/main/java/io/github/acdcjunior/prp/Application.java
@@ -1,9 +1,14 @@
 package io.github.acdcjunior.prp;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.velocity.VelocityProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver;
 
@@ -30,6 +35,17 @@ public class Application {
         // the maximum file size in bytes: 15MB
         resolver.setMaxUploadSize(15_000_000);
         return resolver;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.setDateFormat(new ISO8601DateFormat());
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
     }
 
 }
